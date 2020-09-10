4c495ee54467532c332ccca9d3e52cf4c34f8793
Milind <milindnkm@gmail.com>
2020-09-10 18:37:12 +0530

    End of session 8

---
+++ DatingApp.API/Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using DatingApp.API.Data;
+using DatingApp.API.DTOS;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController :ControllerBase
+    {
+        private readonly IDatingRepository _repo;
+        private readonly IMapper _mapper;
+
+        public UsersController(IDatingRepository repo, IMapper mapper)
+        {
+            _repo = repo;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var user = await _repo.GetUsers();
+            var userToReturn = _mapper.Map<IEnumerable<UserForListDto>>(user);
+            return Ok(userToReturn);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUsers(int id)
+        {
+            var user = await _repo.GetUser(id);
+            var userToReturn = _mapper.Map<UserForDetailsDto>(user);
+            return Ok(userToReturn);
+        }
+
+    }
+}
--- DatingApp.API/Helpers/Extenstion.cs
+++ DatingApp.API/Helpers/Extenstion.cs
@@ -1,13 +1,31 @@
 using Microsoft.AspNetCore.Http;
+using System;
 
 namespace DatingApp.API.Helpers
 {
     public static class Extenstion
     {
+        /// <summary>
+        /// Extenstion method to add the Error header and error message in HttpResponse
+        /// </summary>
+        /// <param name="response">HttpResponse object</param>
+        /// <param name="message">Error message</param>
         public static void AddApplicationError(this HttpResponse response, string message){
             response.Headers.Add("Application-Error",message);
             response.Headers.Add("Access-Control-Expose-Headers","Application-Error");
             response.Headers.Add("Access-Control-Allow-Origin", "*");
+        }
+        /// <summary>
+        /// Extenstion method to calculate Age
+        /// </summary>
+        /// <param name="theDateTime">Pass birth date</param>
+        /// <returns>Returns Age</returns>
+        public static int CalculateAge(this DateTime theDateTime)
+        {
+            var age = DateTime.Today.Year - theDateTime.Year;
+            if (theDateTime.AddYears(age) > DateTime.Today)
+                age--;
+            return age;
         }
     }
 }
